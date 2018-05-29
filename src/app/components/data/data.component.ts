69db436c450e9fe62194081036088611fe116d62
bikeredu <luiseduardo.tellez@gmail.com>
2018-05-28 22:06:22 -0500

    Ajustes form 2

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,11 +11,26 @@ export class DataComponent {
 
   forma: FormGroup;
 
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: 'Eduardo',
+      apellido: 'Tellez'
+    },
+    correo: 'luiseduardo.tellez@gmail.com'
+  };
+
   constructor() {
+    console.log(this.usuario);
     this.forma = new FormGroup( {
-      'nombre': new FormControl('Eduardo'),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+      'nombre': new FormControl( '', [
+                                      Validators.required,
+                                      Validators.minLength(5)
+                                    ] ),
+      'apellido': new FormControl( '', Validators.required ),
+      'correo': new FormControl( '', [
+                                        Validators.required,
+                                        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+                                      ] )
     });
    }
 
